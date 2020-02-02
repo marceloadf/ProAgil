a0025027a3b283d6dfc8bcb0b25f1615d94d03e6
marceloadf <marceloferreira.eng@hotmail.com>
2020-02-02 19:22:26 -0300

    cors configuration allow any

--- ProAgil.WebAPI/Startup.cs
+++ ProAgil.WebAPI/Startup.cs
@@ -30,6 +30,7 @@ namespace ProAgil.WebAPI
             services.AddDbContext<DataContext>(
                 x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +46,8 @@ namespace ProAgil.WebAPI
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseEndpoints(endpoints =>
             {
